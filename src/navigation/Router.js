72c3d64de2c1b7a16cc0638b748a27625f1c52ec
Saikoyomii9 <saikoyomii9@gmail.com>
2022-10-12 10:48:43 -0400

    2022 10 11

---
+++ src/navigation/Router.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from '../screens/Home';
+import TabNavigator from './TabNavigator';
+import AddList from '../screens/AddList';
+import ExistingListScreen from '../screens/ExistingList';
+
+const Stack = createStackNavigator();
+
+const Router = props => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={'Home'}
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name = {'Start Shopping!'} component={TabNavigator}/>
+        <Stack.Screen name = {'Add List'} component = {AddList}/>
+        <Stack.Screen name = {'Existing List'} component = {ExistingListScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
